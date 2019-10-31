008f62eb2daac21539b0a05c3948f3156a43f61c
Andrey Kovalenko <kovalenkoscreen@gmail.com>
2019-10-31 10:34:16 +0300

    добавлена анимация формы

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ var open = document.querySelector(".map-button");
 var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".feedback-close");
 
+var form = popup.querySelector(".feedback-form");
+var name = popup.querySelector("[name=yourname]");
+var mail = popup.querySelector("[name=email]");
+
 open.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
@@ -10,4 +14,14 @@ open.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
-    });
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!name.value ||!mail.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    }
+  });

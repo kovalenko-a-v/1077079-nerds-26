3c27a299323e843ff31e26193140b869fce7ab47
Andrey Kovalenko <kovalenkoscreen@gmail.com>
2019-11-03 00:31:42 +0300

    Защита личного поректа (попытка 3)
    Выполнены ранее не сделанные критерии.

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".feedback-close");
 
 var form = popup.querySelector(".feedback-form");
-var name = popup.querySelector("[name=yourname]");
+var nickname = popup.querySelector("[name=yourname]");
 var mail = popup.querySelector("[name=email]");
 
 open.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-  });
+});
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
     popup.classList.remove("modal-error");
-  });
+});
 
   form.addEventListener("submit", function (evt) {
-    if (!name.value ||!mail.value) {
+    if (!nickname.value ||!mail.value) {
       evt.preventDefault();
       popup.classList.remove("modal-error");
       popup.offsetWidth = popup.offsetWidth;
